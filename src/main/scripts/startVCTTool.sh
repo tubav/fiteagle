ca58e0a8b185e9ba1b7c72c2a412a5f6dbe15cb3
Alexander Willner <willner@services.av.tu-berlin.de>
2012-07-18 15:41:52 +0200

    fixed issues on Linux

--- src/main/scripts/startVCTTool.sh
+++ src/main/scripts/startVCTTool.sh
@@ -1,1 +1,4 @@
-java -XstartOnFirstThread -jar target/vcttool-0.1/vct-0.1.jar
+#!/bin/bash
+_PARAMS=""
+[ $(uname) == "Darwin" ] && _PARAMS="${_PARAMS} -XstartOnFirstThread"
+java ${_PARAMS} -jar target/vcttool-0.1/vct-0.1.jar
