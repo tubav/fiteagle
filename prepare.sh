4108bf9d16fc9c7c6e04d799e73f0a3114760f08
Alex <alexander.willner@tu-berlin.de>
2012-07-18 19:54:46 +0200

    own teagle maven repository

--- prepare.sh
+++ prepare.sh
@@ -8,11 +8,11 @@ cp lib/broker.openpe.pem1_input_utils-1.0-SNAPSHOT.jar ~/.m2/repository/de/fhg/f
 mkdir -p ~/.m2/repository/de/fhg/fokus/ngni/openpe/openpe.pem1/0.0.6-SNAPSHOT
 cp lib/openpe.pem1-0.0.6-20110617.085800-3.jar ~/.m2/repository/de/fhg/fokus/ngni/openpe/openpe.pem1/0.0.6-SNAPSHOT/openpe.pem1-0.0.6-SNAPSHOT.jar
 
-mkdir -p ~/.m2/repository/de/fhg/fokus/ngni/teagle/model/0.0.1-SNAPSHOT
-cp lib/model-0.0.1-SNAPSHOT.jar ~/.m2/repository/de/fhg/fokus/ngni/teagle/model/0.0.1-SNAPSHOT/model-0.0.1-SNAPSHOT.jar
+#mkdir -p ~/.m2/repository/de/fhg/fokus/ngni/teagle/model/0.0.1-SNAPSHOT
+#cp lib/model-0.0.1-SNAPSHOT.jar ~/.m2/repository/de/fhg/fokus/ngni/teagle/model/0.0.1-SNAPSHOT/model-0.0.1-SNAPSHOT.jar
 
-mkdir -p ~/.m2/repository/de/fhg/fokus/ngni/teagle/tssgrepoclient/0.0.1-SNAPSHOT
-cp lib/tssgrepoclient-0.0.1-SNAPSHOT.jar ~/.m2/repository/de/fhg/fokus/ngni/teagle/tssgrepoclient/0.0.1-SNAPSHOT/tssgrepoclient-0.0.1-SNAPSHOT.jar
+#mkdir -p ~/.m2/repository/de/fhg/fokus/ngni/teagle/tssgrepoclient/0.0.1-SNAPSHOT
+#cp lib/tssgrepoclient-0.0.1-SNAPSHOT.jar ~/.m2/repository/de/fhg/fokus/ngni/teagle/tssgrepoclient/0.0.1-SNAPSHOT/tssgrepoclient-0.0.1-SNAPSHOT.jar
 
-mkdir -p ~/.m2/repository/de/fhg/fokus/ngni/teagle/util/0.0.1-SNAPSHOT
-cp lib/util-0.0.1-SNAPSHOT.jar ~/.m2/repository/de/fhg/fokus/ngni/teagle/util/0.0.1-SNAPSHOT/util-0.0.1-SNAPSHOT.jar
+#mkdir -p ~/.m2/repository/de/fhg/fokus/ngni/teagle/util/0.0.1-SNAPSHOT
+#cp lib/util-0.0.1-SNAPSHOT.jar ~/.m2/repository/de/fhg/fokus/ngni/teagle/util/0.0.1-SNAPSHOT/util-0.0.1-SNAPSHOT.jar
