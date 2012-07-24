7b96ba36d2071d78a825a49fd63807d77837c609
Alex <alexander.willner@tu-berlin.de>
2012-07-24 09:07:42 +0200

    automatically invoke mvn package

--- clients/vcttool/src/main/scripts/startVCTTool.sh
+++ clients/vcttool/src/main/scripts/startVCTTool.sh
@@ -3,6 +3,6 @@ _PARAMS=""
 _JAR="target/vcttool-jar-with-dependencies.jar"
 
 [ $(uname) == "Darwin" ] && _PARAMS="${_PARAMS} -XstartOnFirstThread"
-[ ! -f "${_JAR}" ] && echo "Run 'mvn package' first!" && exit 1
+[ ! -f "${_JAR}" ] && echo "Running 'mvn package' first..." && mvn package
 
 java ${_PARAMS} -jar ${_JAR}
