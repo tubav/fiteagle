42581a117b2aade3a878934b61a58a45d84490be
dnehls <dxnehls@gmail.com>
2013-03-07 09:16:06 +0100

    created servlet class

---
+++ delivery/xmlrpc/src/main/java/org/fiteagle/api/xmlrpc/SFAXmlRpcServlet.java
@@ -0,0 +1,15 @@
+package org.fiteagle.api.xmlrpc;
+
+import org.apache.xmlrpc.webserver.XmlRpcServlet;
+
+public class SFAXmlRpcServlet extends XmlRpcServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4349365825732565723L;
+
+	
+	
+	
+}
