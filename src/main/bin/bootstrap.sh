84cc044d3a840517eb05a0832c567b18690781ab
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-03-15 07:50:13 +0100

    new bootstrap scripts

---
+++ src/main/bin/bootstrap.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+echo "Usage: bootstrap.sh [<directory>]"
+echo ""
+
+if [ -n "$1" ]; then _target="$1"; else _target="./fiteagle"; fi
+_target_base="`dirname ${_target}`"
+_log="`mktemp -t fiteagle`"
+
+function checkBinary {
+  echo -n " * Checking for '$1'..."
+  if command -v $1 >/dev/null 2>&1; then
+     echo "OK"
+     return 0
+   else
+     echo >&2 "FAILED."
+     return 1
+   fi
+}
+
+function checkEnvironment {
+  _error=0
+  echo "Checking environment..."
+  checkBinary java; _error=$(($_error + $?))
+  checkBinary javac; _error=$(($_error + $?))
+  checkBinary mvn; _error=$(($_error + $?))
+  checkBinary git; _error=$(($_error + $?))
+  checkFITeagleInstallation; _error=$(($_error + $?))
+  if [ "0" != "$_error" ]; then
+    echo >&2 "FAILED. Please install the above mentioned binaries."
+    exit 1
+  fi
+}
+
+function checkFITeagleInstallation {
+  echo -n " * Checking for 'FITeagle'..."
+  if [ -x "${_target}/src/main/bin/fiteagle.sh" ]; then
+    if [ -w "${_target}/" ]; then 
+      echo "OK (found at '${_target}')"
+    else
+      echo "FAILED. (no write permissions in '${_target}')"
+      return 1
+    fi
+  else
+    if [ -w "${_target_base}" ]; then 
+      echo "OK (will install it to '${_target}')"
+    else
+      echo "FAILED (no write permissions to create '${_target}')"
+      return 1
+    fi
+  fi
+  return 0
+}
+
+function installFITeagle {
+  repo="fiteagle"
+  git_url="git://github.com/tubav/${repo}"
+  
+  if [ -d "${_target}/.git" ]; then
+    echo -n "Updating FITeagle..."
+    (cd "${_target}" && git pull -q)
+  else
+    echo -n "Installing FITeagle..."
+    git clone -q --recursive --depth 1 ${git_url} ${_target}
+  fi
+  if [ "0" != "$?" ]; then
+    echo >&2 "FAILED. Please have a look above."
+    exit 2
+  fi
+  
+  echo "OK"
+}
+
+checkEnvironment
+installFITeagle
+
+cd ${_target}
+./src/main/bin/fiteagle.sh test
+./src/main/bin/fiteagle.sh start
--- src/main/bin/fiteagle.sh
+++ src/main/bin/fiteagle.sh
@@ -1,65 +1,22 @@
 #!/usr/bin/env bash
-
-function checkBinary {
-  echo -n " * Checking for '$1'..."
-  if command -v $1 >/dev/null 2>&1; then
-     echo "OK"
-     return 0
-   else
-     echo >&2 "FAILED."
-     return 1
-   fi
-}
-
-function checkEnvironment {
-  _error=0
-  echo "Checking environment..."
-  checkBinary java; _error=$(($_error + $?))
-  checkBinary javac; _error=$(($_error + $?))
-  checkBinary mvn; _error=$(($_error + $?))
-  checkBinary git; _error=$(($_error + $?))
-  if [ "0" != "$_error" ]; then
-    echo >&2 "FAILED. Please install the above mentioned binaries."
-    exit 1
-  fi
-}
-
-function installFITeagle {
-  _git_target="fiteagle"
-  _git_url="git://github.com/tubav/${_git_target}"
-
-  if [ -d "${_git_target}" ]; then
-    echo "Updating FITeagle..."
-    (cd "${_git_target}" && git pull)
-  else
-    echo "Installing FITeagle..."
-    git clone --recursive --depth 1 ${_git_url}
-  fi
-  
-  if [ "0" != "$?" ]; then
-    echo >&2 "FAILED. Please have a look above."
-    exit 2
-  fi
-}
-
-function testFITeagle {
-  _target="fiteagle"
-  
-  echo "Testing FITeagle..."
-  cd "${_target}" && mvn test
+function testFITeagle {  
+  echo -n "Testing FITeagle (this might take a while)..."
+  mvn -q test
   if [ "0" != "$?" ]; then
     echo >&2 "FAILED. Please have a look above."
     exit 3
   fi
+  
+  echo "OK"
 }
 
 function runFITeagle {
-  _target="fiteagle/delivery/xmlrpc"
-  
+  main_dir="./delivery/xmlrpc"  
   echo "Starting FITeagle..."
-  cd "${_target}"
-  [ -e target/jetty-ssl.keystore ] || ./src/main/bin/ssl_create_server_cert.sh
-  mvn jetty:run
+  cd "${main_dir}"
+  [ -x ./src/main/bin/ssl_create_server_cert.sh ] || { echo "ERROR 5"; exit 5; }
+  [ -f target/jetty-ssl.keystore ] || ./src/main/bin/ssl_create_server_cert.sh
+  mvn -q jetty:run
   if [ "0" != "$?" ]; then
     echo >&2 "FAILED. Please have a look above."
     exit 3
@@ -67,12 +24,10 @@ function runFITeagle {
 }
 
 function usage {
-    echo "Usage: $0 install|update|test|start"
+    echo "Usage: ./src/main/bin/fiteagle.sh test|start"
 }
 
 case $1 in
-    install) checkEnvironment; installFITeagle;;
-    update) checkEnvironment; installFITeagle;;
     test) testFITeagle;;
     start) runFITeagle;;
     *) usage
