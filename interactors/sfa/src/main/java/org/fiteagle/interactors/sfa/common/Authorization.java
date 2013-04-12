f4bfae37fd724929af047e8a50b91d24f675935b
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-04-12 12:07:32 +0200

    Fixed a warning.

--- interactors/sfa/src/main/java/org/fiteagle/interactors/sfa/common/Authorization.java
+++ interactors/sfa/src/main/java/org/fiteagle/interactors/sfa/common/Authorization.java
@@ -32,7 +32,7 @@ public class Authorization {
 	public void checkCredentialsList(ListCredentials listCredentials) {
 		
 		ArrayList<Credentials> credentialList = listCredentials.getCredentialsList();
-		for (Iterator iterator = credentialList.iterator(); iterator.hasNext();) {
+		for (Iterator<Credentials> iterator = credentialList.iterator(); iterator.hasNext();) {
 			Credentials credentials = (Credentials) iterator.next();
 			this.checkCredentialTypeAndVersion(credentials);
 			if (!this.areCredentialTypeAndVersionValid()) {
