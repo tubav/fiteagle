96775ee29e6d177fa42479b06041433f22ff3285
Mitja <mitjanikolaus@gmail.com>
2013-05-06 19:39:55 +0200

    forgot to add the UserDBManager..

--- core/src/main/java/org/fiteagle/core/config/FiteaglePreferences.java
+++ core/src/main/java/org/fiteagle/core/config/FiteaglePreferences.java
@@ -1,9 +1,7 @@
 package org.fiteagle.core.config;
 
 public abstract class FiteaglePreferences {
-  
-  
-  
+    
   public abstract void put(String key, String value);
   
   public abstract String get(String key); 
