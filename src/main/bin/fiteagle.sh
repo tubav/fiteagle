03445439ec02a955e5d55d542fcb5192a4fdf112
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-05-07 13:45:17 +0200

    show log file while testing

--- src/main/bin/fiteagle.sh
+++ src/main/bin/fiteagle.sh
@@ -3,7 +3,7 @@
 _log="`mktemp -t fiteagle.XXXXX`"
 
 function testFITeagle {  
-  echo "Testing and bootstraping FITeagle (this might take a while)..."
+  echo "Testing and bootstraping FITeagle (this might take a while - have a look at '${_log}')..."
   mvn -B install > "${_log}" 2>&1
   if [ "0" != "$?" ]; then
     echo >&2 "FAILED. Please have a look at '${_log}'."
