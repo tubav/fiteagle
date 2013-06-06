09bf1655b88bd9084e429ca82beadf0cc020be1f
mitjanikolaus <mitjanikolaus@gmail.com>
2013-06-06 18:51:20 +0200

    added Cookie authentication support

--- delivery/interfaces/src/main/java/org/fiteagle/delivery/rest/AuthenticationFilter.java
+++ delivery/interfaces/src/main/java/org/fiteagle/delivery/rest/AuthenticationFilter.java
@@ -1,12 +1,19 @@
+
+
 package org.fiteagle.delivery.rest;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.xml.bind.DatatypeConverter;
 
 import org.fiteagle.core.userdatabase.UserDB.DatabaseException;
@@ -15,43 +22,65 @@ import org.fiteagle.core.userdatabase.UserDBManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sun.jersey.core.util.Base64;
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
 
-public class AuthenticationFilter implements ContainerRequestFilter{
+public class AuthenticationFilter implements ContainerRequestFilter, ContainerResponseFilter{
 
   @Context
-  private HttpServletRequest request;
+  private HttpServletRequest request;  
+  
+  private final static String COOKIE_NAME = "fiteagle_user_cookie";
+  private HashMap<String, Cookie> cookies = new HashMap<>();
   
   private UserDBManager manager = null;
+  
   private Logger log = LoggerFactory.getLogger(getClass());
   
-	@Override
-	public ContainerRequest filter(ContainerRequest req) {
-	  if(!req.getSecurityContext().isSecure()){
-	    throw new WebApplicationException(Response.Status.BAD_REQUEST);
-	  }
-	  
-		String method = req.getMethod();    
+  @Override
+  public ContainerRequest filter(ContainerRequest req) {
+    if(!req.getSecurityContext().isSecure()){
+      throw new WebApplicationException(Response.Status.BAD_REQUEST);
+    }
+    
+    String method = req.getMethod();    
     if(method.equals("PUT")){
       return req;
     }
-		String auth = req.getHeaderValue("authorization");		
-		String[] credentials = decode(auth);		
-		if(credentials == null || credentials.length != 2){
-			throw new WebApplicationException(Response.Status.UNAUTHORIZED);
-		}
-		
-//		String loggedUser = (String) this.request.getSession().getAttribute("LOGGED_USER");		
-//		if(loggedUser != null && loggedUser.equals(credentials[0])){
-//		  //TODO: safe?
-//		  return req;
-//		}
-		
-		manager = UserDBManager.getInstance();
-		try {
+    
+    if(!authenticateWithCookie(req)){
+      authenticateWithUsernameAndPassword(req);
+    }
+    
+    return req;
+  }
+  
+  private String[] decode(String auth){ 
+    if(auth == null || (!auth.startsWith("Basic ") && !auth.startsWith("basic "))){
+      throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+    }
+    auth = auth.replaceFirst("[B|b]asic ", "");
+    byte[] decoded = DatatypeConverter.parseBase64Binary(auth);
+    if(decoded == null || decoded.length == 0){
+      throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+    }
+    return new String(decoded).split(":", 2);
+  }
+  
+  private void authenticateWithUsernameAndPassword(ContainerRequest req){
+    String auth = req.getHeaderValue("authorization");    
+    String[] credentials = decode(auth);    
+    if(credentials == null || credentials.length != 2){
+      throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+    }
+    
+    manager = UserDBManager.getInstance();
+    try {
       if(!manager.verifyCredentials(credentials[0], credentials[1])){
-      	throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+        throw new WebApplicationException(Response.Status.UNAUTHORIZED);
       }
     } catch (NoSuchAlgorithmException | IOException | DatabaseException e) {
         log.error(e.getMessage());
@@ -60,26 +89,47 @@ public class AuthenticationFilter implements ContainerRequestFilter{
       throw new WebApplicationException(Response.Status.UNAUTHORIZED);       
     }
 
-		if(!req.getPathSegments().get(1).getPath().equals(credentials[0])){
-		    throw new WebApplicationException(Response.Status.UNAUTHORIZED); 
-		}
-		
-		//TODO: send cookie back?
-		//System.out.println(request.getSession().getId());
-		//this.request.getSession().setAttribute("LOGGED_USER", credentials[0]);
+    if(!req.getPathSegments().get(1).getPath().equals(credentials[0])){
+        throw new WebApplicationException(Response.Status.FORBIDDEN); 
+    }
+  }
 
-		return req;
-	}
-	
-	private String[] decode(String auth){	
-		if(auth == null || (!auth.startsWith("Basic ") && !auth.startsWith("basic "))){
-			throw new WebApplicationException(Response.Status.UNAUTHORIZED);
-		}
-		auth = auth.replaceFirst("[B|b]asic ", "");
-		byte[] decoded = DatatypeConverter.parseBase64Binary(auth);
-		if(decoded == null || decoded.length == 0){
-			throw new WebApplicationException(Response.Status.UNAUTHORIZED);
-		}
-		return new String(decoded).split(":", 2);
-	}
+  private boolean authenticateWithCookie(ContainerRequest req){
+    String username = (String) req.getPathSegments().get(1).getPath(); 
+    Cookie cookie = req.getCookies().get(COOKIE_NAME);
+    Cookie cookieFromStorage = cookies.get(username);
+    if(cookie != null && cookieFromStorage != null && cookie.getValue().equals(cookieFromStorage.getValue())){
+      return true;
+    }
+    return false;
+  }
+  
+  @Override
+  public ContainerResponse filter(ContainerRequest req, ContainerResponse resp) {
+    if(req.getMethod().equals("PUT")){
+      return resp;
+    }
+        
+    String username = (String) req.getPathSegments().get(1).getPath(); 
+    if(username != null && req.getCookies().get(COOKIE_NAME) == null){
+      ResponseBuilder rb = Response.fromResponse(resp.getResponse());
+      rb.cookie(createNewCookie(username));
+      resp.setResponse(rb.build());
+    }
+    
+    //TODO: when to remove cookie?
+    return resp;
+  }
+  
+  private NewCookie createNewCookie(String username){
+    String authToken = createRandomAuthToken();
+    NewCookie cookie = new NewCookie(COOKIE_NAME, authToken, null, null , null, 1800, true);
+    cookies.put(username, cookie.toCookie());
+    return cookie;
+  }
+
+  private String createRandomAuthToken() {
+    String u = UUID.randomUUID().toString();    
+    return new String(Base64.encode(u.getBytes()));
+  }
 }
