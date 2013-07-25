99272cc65d3236d942330c1b2ef73cb7f583274e
ozanoo <ozanoezpehlivan@gmail.com>
2013-07-25 16:26:07 +0200

    gui modifications, added sort status result

--- delivery/interfaces/src/main/webapp/gui/js/statusPage.js
+++ delivery/interfaces/src/main/webapp/gui/js/statusPage.js
@@ -26,6 +26,19 @@ function(){
 	}
 	
 	showStatus = function(data){
+		//sort the data list
+//		data = data.sort(sortStatusLines);
+		
+//		data.sort(function(a,b){
+//			if (a.id < b.id)
+//				  return -1;
+//				if (a.id > b.id )
+//				  return 1;
+//				return 0;
+////				return a.id.localeCompare(b.id);
+//				});
+		
+		sortStatusLines(data);
 		
 		for(var k in data){
 			console.log(data[k].id);
@@ -34,6 +47,8 @@ function(){
 	}
 	
 	showDetailedStatus = function(data) {
+		//sort the data list
+		sortStatusLines(data.components);
 		var table =  "<table><caption><h3>"+data.id+"</h3></caption><thead><tr><th>Monitoring Status</th><th>Component Name</th><th>Last checked</th></tr></thead><tbody>";
 		var tableBody = "";
 		for(var k in data.components){
@@ -47,6 +62,32 @@ function(){
 			
 		
 	}
+	
+	sortStatusLines = function(data){
+		
+		return data.sort(function(a,b){
+			
+			if(a.status == 'up' && b.status != 'up') return -1;
+			if(a.status == 'upAndLastCheckedOld' && b.status != 'up' && b.status != 'upAndLastCheckedOld') return -1;
+			if(a.status == 'partially' && b.status != 'up' && b.status != 'upAndLastCheckedOld' && b.status != 'partially') return -1;
+			if(a.status == 'down' && b.status != 'up' && b.status != 'upAndLastCheckedOld' && b.status != 'partially' && b.status != 'down') return -1;
+			
+			if(a.status == b.status){
+				if (a.id < b.id)
+					return -1;
+				if (a.id > b.id )
+					return 1;
+				return 0;
+			}
+			
+			return 1;
+//				return a.id.localeCompare(b.id);
+				});
+		}
+	
+	
+	
+	
 	buildRow = function(testbedstatus){
 //		var row = '<tr><td>'+getStatusIcon(testbedstatus.status)+'</td><td>'+testbedstatus.id+'</td><td>'+new Date(testbedstatus.lastCheck).toString()+'</td><td class="statusRow" '+'id="'+testbedstatus.id+'">Details</td></tr>';
 		var lastCheckStr = 'unknown'; 
