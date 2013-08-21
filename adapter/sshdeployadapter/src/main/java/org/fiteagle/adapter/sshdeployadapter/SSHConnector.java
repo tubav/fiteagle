cb55523ec8e76fd022af72f2865c8558913fb030
dnehls <daniel.nehls@tu-berlin.de>
2013-08-21 15:54:32 +0200

    added ssh deployment to sshdeployadapter

---
+++ adapter/sshdeployadapter/src/main/java/org/fiteagle/adapter/sshdeployadapter/SSHConnector.java
@@ -0,0 +1,212 @@
+package org.fiteagle.adapter.sshdeployadapter;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.security.PublicKey;
+import java.util.concurrent.TimeUnit;
+
+import org.fiteagle.adapter.common.AdapterConfiguration;
+
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.common.IOUtils;
+import net.schmizz.sshj.connection.ConnectionException;
+import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.connection.channel.direct.Session.Command;
+import net.schmizz.sshj.transport.TransportException;
+import net.schmizz.sshj.transport.verification.HostKeyVerifier;
+
+public class SSHConnector {
+	  private String ip="";
+	  private String username="";
+	  private String password="";
+	  private String port;
+	
+	
+	private SSHClient client;
+	AdapterConfiguration adapterConfiguration ;
+	public SSHConnector(String ip,String port, String username, String password,AdapterConfiguration adapterConfiguration) {
+		this.ip = ip;
+		this.port = port;
+		this.username = username;
+		this.password = password;
+		this.adapterConfiguration = adapterConfiguration;
+	}
+
+	public void connect(){
+		
+		client = new SSHClient();
+		InetAddress host;
+		try {
+			host = InetAddress.getByName(ip);
+			client.addHostKeyVerifier(new HostKeyVerifier() {
+				
+				public boolean verify(String arg0, int arg1, PublicKey arg2) {
+					// TODO Auto-generated method stub
+					return true;
+				}
+			});
+			client.connect(host);
+			client.authPassword(username, password);
+			this.client = client;
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+	public void createUserAccount(String newUser) {
+		try {
+			Session session = client.startSession();
+			try{
+			Command cmd = session.exec("echo "+password+"| sudo -S adduser -gecos \""+newUser+" "+ newUser+", test@test.test\" -disabled-password "+newUser+" ");
+			cmd.join(5, TimeUnit.SECONDS);
+			System.out.println(IOUtils.readFully(cmd.getInputStream()).toString());
+
+			System.out.println("\n **exit status: "+cmd.getExitStatus());
+			}finally{
+			session.close();
+			}
+		} catch (ConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	public void disconnect() {
+		try {
+			this.client.disconnect();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void createUserSSHDirectory(String newUser) {
+		try {
+			Session session = client.startSession();
+			try{
+			Command cmd = session.exec("echo "+password+"| sudo -S mkdir /home/"+newUser+"/.ssh");
+			cmd.join(5, TimeUnit.SECONDS);
+			System.out.println(IOUtils.readFully(cmd.getInputStream()).toString());
+			System.out.println("\n **exit status: "+cmd.getExitStatus());
+			
+			}finally{
+			session.close();
+			}
+		} catch (ConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void createAuthorizedKeysFile(String newUser) {
+		try {
+			Session session = client.startSession();
+			try{
+			
+			
+			Command com3 = session.exec("echo "+password+"| sudo -S touch /home/"+newUser+"/.ssh/authorized_keys");
+			System.out.println(IOUtils.readFully(com3.getInputStream()).toString());
+			com3.join(5, TimeUnit.SECONDS);
+			System.out.println("\n **exit status: "+com3.getExitStatus());
+		
+			}finally{
+			session.close();
+			}
+		} catch (ConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	public void changeOwnerOfUserHome(String newUser) {
+		try {
+			Session session = client.startSession();
+			try{
+			
+				
+				Command com2 = session.exec("echo "+password+"| sudo -S chown -R "+newUser+":"+newUser+" /home/"+newUser+"/.ssh");
+				System.out.println(IOUtils.readFully(com2.getInputStream()).toString());
+				com2.join(5, TimeUnit.SECONDS);
+				System.out.println("\n **exit status: "+com2.getExitStatus());
+			
+			
+			}finally{
+			session.close();
+			}
+		} catch (ConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void addSSHKey(String sshKey, String newUser) {
+		try {
+			Session session = client.startSession();
+			try{
+			
+				Command com4 = session.exec("echo "+password+"| sudo -S bash -c 'echo "+sshKey+" >> /home/"+newUser+"/.ssh/authorized_keys'" );
+				System.out.println(IOUtils.readFully(com4.getInputStream()).toString());
+				com4.join(5, TimeUnit.SECONDS);
+				System.out.println("\n **exit status: "+com4.getExitStatus());
+				
+				
+			
+			}finally{
+			session.close();
+			}
+		} catch (ConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+}
--- adapter/sshdeployadapter/src/main/java/org/fiteagle/adapter/sshdeployadapter/SSHDeployAdapter.java
+++ adapter/sshdeployadapter/src/main/java/org/fiteagle/adapter/sshdeployadapter/SSHDeployAdapter.java
@@ -5,257 +5,292 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.fiteagle.adapter.common.AdapterConfiguration;
+import org.fiteagle.adapter.common.AdapterUser;
 import org.fiteagle.adapter.common.ResourceAdapter;
 import org.fiteagle.adapter.common.SSHAccessable;
 
-public class SSHDeployAdapter extends ResourceAdapter implements SSHAccessable{
-  private static boolean loaded = false;
-  private String hardwareType;
-  private String ip="";
-  private String username="";
-  private String password="";
-  private String sshKey="";
-  
-  private SSHDeployAdapterConfiguration sshDeployAdapterConfig=SSHDeployAdapterConfiguration.getInstance();
-private String port;
-private AdapterConfiguration adapterConfiguration;
-  
-  public SSHDeployAdapter(){
-	  
-//	  TODO: set staff over properties call other deploy adapter
-    super();
-//    this.setType("org.fiteagle.adapter.sshdeployadapter.SSHDeployAdapter");
-//    this.create();
-//	  SSHDeployAdapter(sshDeployAdapterConfig.get, String username, String password, String sshKey);
-//	  this.setHardwareType(hardwareType); //TODO get hardware type from configuration
-  }
-  
-  public SSHDeployAdapter(String ip,String port, String username, String password, String sshKey){
-	    super();
-	    this.setType("org.fiteagle.adapter.sshdeployadapter.SSHDeployAdapter");
-	    this.setIp(ip);
-	    this.setPort(port);
-	    this.setUsername(username);
-	    this.setPassword(password);
-	    this.setSshKey(sshKey);
-	  }
+public class SSHDeployAdapter extends ResourceAdapter implements SSHAccessable {
+	private static boolean loaded = false;
+	private String hardwareType;
+	private String ip = "";
+	private String username = "";
+	private String password = "";
+	private String sshKey = "";
 
-  
+	private SSHDeployAdapterConfiguration sshDeployAdapterConfig = SSHDeployAdapterConfiguration
+			.getInstance();
+	private String port;
+	private AdapterConfiguration adapterConfiguration;
 
+	public SSHDeployAdapter() {
 
-@Override
-  public void start() {
-    // TODO Auto-generated method stub
-    
-  }
+		// TODO: set staff over properties call other deploy adapter
+		super();
+		// this.setType("org.fiteagle.adapter.sshdeployadapter.SSHDeployAdapter");
+		// this.create();
+		// SSHDeployAdapter(sshDeployAdapterConfig.get, String username, String
+		// password, String sshKey);
+		// this.setHardwareType(hardwareType); //TODO get hardware type from
+		// configuration
+	}
 
-  @Override
-  public void stop() {
-    // TODO Auto-generated method stub
-    
-  }
+	public SSHDeployAdapter(String ip, String port, String username,
+			String password, String sshKey) {
+		super();
+		this.setType("org.fiteagle.adapter.sshdeployadapter.SSHDeployAdapter");
+		this.setIp(ip);
+		this.setPort(port);
+		this.setUsername(username);
+		this.setPassword(password);
+		this.setSshKey(sshKey);
+	}
 
-  @Override
-  public void create() {
-    // TODO Auto-generated method stub
-    
-  }
+	@Override
+	public void start() {
 
-  @Override
-  public void release() {
-    // TODO Auto-generated method stub
-    
-  }
+		if (adapterConfiguration != null) {
+			SSHConnector connector = new SSHConnector(ip, port, username,
+					password, adapterConfiguration);
+			for (AdapterUser user : adapterConfiguration.getUsers()) {
+				String newUser = user.getUsername();
+				
+				connector.connect();
+				connector.createUserAccount(newUser);
 
-  @Override
-  public String getHardwareType() {
-    return hardwareType;
-  }
+				connector.createUserSSHDirectory(newUser);
+				connector.createAuthorizedKeysFile(newUser);
+				connector.changeOwnerOfUserHome(newUser);
+				for(String userPublicKey: user.getSshPublicKeys()){
+					connector.addSSHKey(userPublicKey, newUser);
+				}
+			}
+			connector.disconnect();
+		}
 
-  @Override
-  public void setHardwareType(String hardwareType) {
-    this.hardwareType = hardwareType;
-  }
-@Override 
-public String getIp() {
-	return ip;
-}
+	}
 
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
 
-@Override
-public void setIp(String ip) {
-	this.ip = ip;
-}
+	}
 
-@Override
-public String getPort(){
-	return this.port;
-}
-@Override
-public void setPort(String port) {
-	this.port = port;
-	
-}
-@Override
-public String getUsername() {
-	return username;
-}
-@Override
-public void setUsername(String username) {
-	this.username = username;
-}
-@Override
-public String getPassword() {
-	return password;
-}
-@Override
-public void setPassword(String password) {
-	this.password = password;
-}
-@Override
-public String getSshKey() {
-	return sshKey;
-}
-@Override
-public void setSshKey(String sshKey) {
-	this.sshKey = sshKey;
-}
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
 
-@Override
-public List<ResourceAdapter> getJavaInstances() {
-	List<ResourceAdapter> resourceAdapters = new ArrayList<ResourceAdapter>();
-	
-	
-	String[] ips = null;
-	String[] ports = null;
-	String[] usernames = null;
-	String[] passwords = null;
-	String[] sshKeys = null;
-	String[] countries = null;
-	String[] latitudes = null;
-	String[] longitudes = null;
-	String[] hardwareTypes = null;
-	
-	if (sshDeployAdapterConfig.getPasswords()==null && sshDeployAdapterConfig.getSsh_keys()==null || sshDeployAdapterConfig.getIps()==null || sshDeployAdapterConfig.getUsernames()==null)
+	}
+
+	@Override
+	public void release() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getHardwareType() {
+		return hardwareType;
+	}
+
+	@Override
+	public void setHardwareType(String hardwareType) {
+		this.hardwareType = hardwareType;
+	}
+
+	@Override
+	public String getIp() {
+		return ip;
+	}
+
+	@Override
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	@Override
+	public String getPort() {
+		return this.port;
+	}
+
+	@Override
+	public void setPort(String port) {
+		this.port = port;
+
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String getSshKey() {
+		return sshKey;
+	}
+
+	@Override
+	public void setSshKey(String sshKey) {
+		this.sshKey = sshKey;
+	}
+
+	@Override
+	public List<ResourceAdapter> getJavaInstances() {
+		List<ResourceAdapter> resourceAdapters = new ArrayList<ResourceAdapter>();
+
+		String[] ips = null;
+		String[] ports = null;
+		String[] usernames = null;
+		String[] passwords = null;
+		String[] sshKeys = null;
+		String[] countries = null;
+		String[] latitudes = null;
+		String[] longitudes = null;
+		String[] hardwareTypes = null;
+
+		if (sshDeployAdapterConfig.getPasswords() == null
+				&& sshDeployAdapterConfig.getSsh_keys() == null
+				|| sshDeployAdapterConfig.getIps() == null
+				|| sshDeployAdapterConfig.getUsernames() == null)
+			return resourceAdapters;
+
+		if (sshDeployAdapterConfig.getIps() != null) {
+			ips = sshDeployAdapterConfig.getIps().split(",");
+		}
+
+		if (sshDeployAdapterConfig.getUsernames() != null) {
+			usernames = sshDeployAdapterConfig.getUsernames().split(",");
+		}
+
+		if (sshDeployAdapterConfig.getPasswords() != null) {
+			passwords = sshDeployAdapterConfig.getPasswords().split(",");
+		}
+
+		if (sshDeployAdapterConfig.getSsh_keys() != null) {
+			sshKeys = sshDeployAdapterConfig.getSsh_keys().split(",");
+		}
+
+		if (sshDeployAdapterConfig.getCountries() != null) {
+			countries = sshDeployAdapterConfig.getCountries().split(",");
+		}
+
+		if (sshDeployAdapterConfig.getLatitudes() != null) {
+			latitudes = sshDeployAdapterConfig.getLatitudes().split(",");
+		}
+
+		if (sshDeployAdapterConfig.getLongitues() != null) {
+			longitudes = sshDeployAdapterConfig.getLongitues().split(",");
+		}
+
+		if (sshDeployAdapterConfig.getHardwareTypes() != null) {
+			hardwareTypes = sshDeployAdapterConfig.getHardwareTypes()
+					.split(",");
+		}
+		if (sshDeployAdapterConfig.getPorts() != null) {
+			ports = sshDeployAdapterConfig.getPorts().split(",");
+		}
+
+		if (!(ips.length == usernames.length && usernames.length == passwords.length)
+				&& (!(ips.length == usernames.length && usernames.length == sshKeys.length)))
+			return resourceAdapters;
+
+		for (int i = 0; i < usernames.length; i++) {
+			SSHDeployAdapter sshDeployAdapter = new SSHDeployAdapter(
+					ips[i].trim(), ports[i].trim(), usernames[i].trim(),
+					passwords[i].trim(), sshKeys[i].trim());
+
+			if (hardwareTypes != null && i < hardwareTypes.length)
+				sshDeployAdapter.setHardwareType(hardwareTypes[i].trim());
+			if (countries != null && i < countries.length)
+				sshDeployAdapter.addProperty("country", countries[i].trim());
+			if (latitudes != null && i < latitudes.length)
+				sshDeployAdapter.addProperty("latitude", latitudes[i].trim());
+			if (longitudes != null && i < longitudes.length)
+				sshDeployAdapter.addProperty("longitude", longitudes[i].trim());
+
+			sshDeployAdapter.setExclusive(true);
+			resourceAdapters.add(sshDeployAdapter);
+		}
+
 		return resourceAdapters;
-	
-	if (sshDeployAdapterConfig.getIps()!=null) {
-		ips = sshDeployAdapterConfig.getIps().split(",");
 	}
-	
-	if(sshDeployAdapterConfig.getUsernames()!=null){
-		usernames = sshDeployAdapterConfig.getUsernames().split(",");
-	}
-	
-	if (sshDeployAdapterConfig.getPasswords()!=null) {
-		passwords = sshDeployAdapterConfig.getPasswords().split(",");
-	}
-	
-	if (sshDeployAdapterConfig.getSsh_keys()!=null) {
-		sshKeys = sshDeployAdapterConfig.getSsh_keys().split(",");
-	}
-	
-	if (sshDeployAdapterConfig.getCountries()!=null) {
-		countries = sshDeployAdapterConfig.getCountries().split(",");
-	}
-	
-	if (sshDeployAdapterConfig.getLatitudes()!=null) {
-		latitudes = sshDeployAdapterConfig.getLatitudes().split(",");
-	}
-	
-	if (sshDeployAdapterConfig.getLongitues()!=null) {
-		longitudes = sshDeployAdapterConfig.getLongitues().split(",");
-	}
-	
-	if (sshDeployAdapterConfig.getHardwareTypes()!=null) {
-		hardwareTypes = sshDeployAdapterConfig.getHardwareTypes().split(",");
-	}
-	if (sshDeployAdapterConfig.getPorts()!=null) {
-		ports = sshDeployAdapterConfig.getPorts().split(",");
-	}
-	
-	
-	if(!(ips.length==usernames.length && usernames.length==passwords.length)&&(!(ips.length==usernames.length && usernames.length==sshKeys.length)))
-	return resourceAdapters;
-	
-	for (int i = 0; i < usernames.length; i++) {
-		SSHDeployAdapter sshDeployAdapter = new SSHDeployAdapter(ips[i].trim(),ports[i].trim(), usernames[i].trim(), passwords[i].trim(), sshKeys[i].trim());
-		
-		if(hardwareTypes!=null && i<hardwareTypes.length)
-		sshDeployAdapter.setHardwareType(hardwareTypes[i].trim());
-		if(countries!=null && i<countries.length)
-		sshDeployAdapter.addProperty("country", countries[i].trim());
-		if(latitudes!=null && i<latitudes.length)
-		sshDeployAdapter.addProperty("latitude", latitudes[i].trim());
-		if(longitudes!=null && i<longitudes.length)
-		sshDeployAdapter.addProperty("longitude", longitudes[i].trim());
-		
-		sshDeployAdapter.setExclusive(true);
-		resourceAdapters.add(sshDeployAdapter);
-	}
-	
-	return resourceAdapters;
-}
 
-public void setPreferences(String ips, String usernames, String passwords, String hardwareTypesPreference,String sshKeys, String countries, String latitudes, String longitudes){
-	
-	if(ips!=null)
-		sshDeployAdapterConfig.setIps(ips);
+	public void setPreferences(String ips, String usernames, String passwords,
+			String hardwareTypesPreference, String sshKeys, String countries,
+			String latitudes, String longitudes) {
 
-	if(usernames!=null)
-		sshDeployAdapterConfig.setUsernames(usernames);
+		if (ips != null)
+			sshDeployAdapterConfig.setIps(ips);
 
-	if(passwords!=null)
-		sshDeployAdapterConfig.setPasswords(passwords);
+		if (usernames != null)
+			sshDeployAdapterConfig.setUsernames(usernames);
 
-	if(sshKeys!=null)
-		sshDeployAdapterConfig.setSsh_keys(sshKeys);
-	
-	if(countries!=null)
-		sshDeployAdapterConfig.setCountries(countries);
-	
-	if(latitudes!=null)
-		sshDeployAdapterConfig.setLatitudes(latitudes);
+		if (passwords != null)
+			sshDeployAdapterConfig.setPasswords(passwords);
 
-	if(longitudes!=null)
-		sshDeployAdapterConfig.setLongitues(longitudes);
-	
-	if(hardwareTypesPreference!=null)
-		sshDeployAdapterConfig.setHardwareTypes(hardwareTypesPreference);
-}
-public void removeAllPreferences(){
+		if (sshKeys != null)
+			sshDeployAdapterConfig.setSsh_keys(sshKeys);
 
-	sshDeployAdapterConfig.removeIps();
-	
-	sshDeployAdapterConfig.removeUsernames();
-	
-	sshDeployAdapterConfig.removePasswords();
-	
-	sshDeployAdapterConfig.removeSsh_keys();
-	
-	sshDeployAdapterConfig.removeCountries();
-	
-	sshDeployAdapterConfig.removeLatitudes();
-	
-	sshDeployAdapterConfig.removeLongitues();
-	
-	sshDeployAdapterConfig.removeHardwareTypes();
-}
+		if (countries != null)
+			sshDeployAdapterConfig.setCountries(countries);
 
-@Override
-public boolean isLoaded() {
-	return this.loaded;
-}
+		if (latitudes != null)
+			sshDeployAdapterConfig.setLatitudes(latitudes);
 
-@Override
-public void setLoaded(boolean loaded) {
-	this.loaded=loaded;
-}
+		if (longitudes != null)
+			sshDeployAdapterConfig.setLongitues(longitudes);
 
-@Override
-public void configure(AdapterConfiguration configuration) {
-	this.adapterConfiguration = configuration;
-	
-}
+		if (hardwareTypesPreference != null)
+			sshDeployAdapterConfig.setHardwareTypes(hardwareTypesPreference);
+	}
+
+	public void removeAllPreferences() {
+
+		sshDeployAdapterConfig.removeIps();
+
+		sshDeployAdapterConfig.removeUsernames();
+
+		sshDeployAdapterConfig.removePasswords();
+
+		sshDeployAdapterConfig.removeSsh_keys();
+
+		sshDeployAdapterConfig.removeCountries();
+
+		sshDeployAdapterConfig.removeLatitudes();
+
+		sshDeployAdapterConfig.removeLongitues();
+
+		sshDeployAdapterConfig.removeHardwareTypes();
+	}
+
+	@Override
+	public boolean isLoaded() {
+		return this.loaded;
+	}
+
+	@Override
+	public void setLoaded(boolean loaded) {
+		this.loaded = loaded;
+	}
+
+	@Override
+	public void configure(AdapterConfiguration configuration) {
+		this.adapterConfiguration = configuration;
+
+	}
 
 }
