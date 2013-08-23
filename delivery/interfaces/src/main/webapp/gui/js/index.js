57c098a51054786701593a165a54324e557070ca
ozanoo <ozanoezpehlivan@gmail.com>
2013-08-23 11:30:35 +0200

    merge

---
+++ delivery/interfaces/src/main/webapp/gui/js/index.js
@@ -0,0 +1,54 @@
+requirejs.config({
+  shim: {
+    'bootstrap'	: ['jquery','cookie'],
+    'history' : ['jquery'],
+    'ajaxify' : ['history'],
+	'prettyCheckable' : ['jquery'],
+	'scrollTo' : ['jquery']
+  }
+});
+
+define(['jquery','bootstrap','loginPage','mainPage','utils', 'history', 'ajaxify','fileSaver'], 
+
+function($,Bootstrap,LoginPage,MainPage,Utils){
+					
+	$.ajaxSetup({cache:false});	
+
+	if(LoginPage.isUserLoggedIn()){
+		MainPage.load();
+	}else{
+		var rememberedUser = Login.getRememberedUsername();
+		//console.log("REMEMBERED: " + rememberedUser);
+		if(rememberedUser){
+			var user = Server.getUser(rememberedUser);
+			if(user){
+				Utils.setCurrentUser(user);
+				MainPage.load();
+			}
+		}else{
+			LoginPage.load();		
+		}
+	}
+	
+	// new Event listener for end of the window resizing. Called : "resizeEnd"	
+	 $(window).resize(function() {
+        if(this.resizeTO) clearTimeout(this.resizeTO);
+        this.resizeTO = setTimeout(function() {
+            $(this).trigger('resizeEnd');
+        }, 200);
+    });
+});	
+	
+
+	
+
+
+
+
+
+
+
+
+
+
+
