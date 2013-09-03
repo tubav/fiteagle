aeae4e0074b0d5162461e6a82e3f9a3ea65b14f0
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-09-03 17:13:17 +0100

    changed path

--- src/test/bin/runAddUser.sh
+++ src/test/bin/runAddUser.sh
@@ -2,11 +2,12 @@
 
 _username="testuser"
 _password="password"
+_json="./src/test/resources/${_username}.json"
 
 type curl >/dev/null 2>&1 || { echo >&2 "I require curl but it's not installed.  Aborting."; exit 1; }
 
 echo "Addding testuser:"
-curl -k --request PUT "https://localhost:8443/api/v1/user/${_username}" --data @../resources/${_username}.json -H "Content-type: application/json"
+curl -k --request PUT "https://localhost:8443/api/v1/user/${_username}" --data @${_json} -H "Content-type: application/json"
 
 echo "Getting testuser:"
 curl -k "https://localhost:8443/api/v1/user/${_username}" --user ${_username}:${_password}
