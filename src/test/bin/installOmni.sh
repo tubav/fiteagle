5121d34ef316cc8115b3b0085fa405175adffbff
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-09-11 11:12:24 +0200

    added script to install omni on the fly

---
+++ src/test/bin/installOmni.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${_dir}/run.config.sh"
+_version="gcf-2.3.3"
+_url="http://software.geni.net/local-sw/real_download"
+_data="software=${_version}.tar.gz&accept=I+have+read+and+accept+the+GPO+terms+of+service+and+disclaimer"
+
+require curl tar
+
+echo -n "Downloading..."
+curl -s --data ${_data} ${_url}|tar xz -C target
+echo "done"
+
+echo "Now please update your environment:"
+echo "  export OMNI_HOME=$(pwd)/target/${_version}"
+echo "  export PATH=\$OMNI_HOME/src:\$PATH"
