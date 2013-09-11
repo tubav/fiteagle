9d1aba58f088330ba98eb246c59226627dd52e4b
AlexanderWillner <alexander.willner@tu-berlin.de>
2013-09-11 09:29:41 +0200

    don't hard code paths

--- src/test/bin/run.config.sh
+++ src/test/bin/run.config.sh
@@ -2,7 +2,7 @@
 
 _username="testuser"
 _password="Password"
-_json="./src/test/resources/${_username}.json"
+_json="${_dir}/../resources/${_username}.json"
 _host="https://localhost:8443/api/v1/user"
 _cert="/tmp/${_username}.pem"
 
