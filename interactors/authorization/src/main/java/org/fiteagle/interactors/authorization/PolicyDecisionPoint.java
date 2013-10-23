831864e151ba6370386580ea3a73f7d3bafbdc6a
Mitja <mitjanikolaus@gmail.com>
2013-10-23 16:07:09 +0200

    improved policy loading

--- interactors/authorization/src/main/java/org/fiteagle/interactors/authorization/PolicyDecisionPoint.java
+++ interactors/authorization/src/main/java/org/fiteagle/interactors/authorization/PolicyDecisionPoint.java
@@ -1,7 +1,17 @@
 package org.fiteagle.interactors.authorization;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sun.xacml.Indenter;
 import com.sun.xacml.PDP;
@@ -14,6 +24,8 @@ import com.sun.xacml.finder.impl.FilePolicyModule;
 
 public class PolicyDecisionPoint {
   
+  private Logger log = LoggerFactory.getLogger(getClass());
+  
   private static PolicyDecisionPoint instance = new PolicyDecisionPoint();
   
   private PDP pdp;
@@ -23,13 +35,37 @@ public class PolicyDecisionPoint {
   }
   
   private PolicyDecisionPoint(){
-    String userDir = System.getProperty("user.dir");
-    String[] splitted = userDir.split("fiteagle");
-    String pathToPolicy = splitted[0]+"fiteagle/interactors/authorization/src/main/resources/Policy.xml";
+    URL url = this.getClass().getClassLoader().getResource("Policy.xml");
+    String pathToPolicy = url.getPath();
+    
+    if(url.getPath().contains("jar")){
+      //TODO: not the best solution
+      log.info("Loading Policy.xml from jar");
+      try {
+        InputStream is = url.openStream();
+        BufferedReader input = new BufferedReader(new InputStreamReader(is));
+        File file = new File("Policy.xml");
+        file.deleteOnExit();
+        FileOutputStream outputStream = new FileOutputStream(file);
+        
+        int ch = 0;
+        while((ch = input.read()) != -1){
+          outputStream.write(ch);
+        }
+        
+        outputStream.close();
+        input.close();
+        is.close();
+        
+        pathToPolicy = "Policy.xml";
+      } catch (IOException e) {
+       log.error(e.getMessage());
+      }
+    }
+    
     
     FilePolicyModule filePolicyModule = new FilePolicyModule();
-//  for (int i = 0; i < policyFiles.length; i++)
-      filePolicyModule.addPolicy(pathToPolicy);
+    filePolicyModule.addPolicy(pathToPolicy);
       
     PolicyFinder policyFinder = new PolicyFinder();
     Set<FilePolicyModule> policyModules = new HashSet<FilePolicyModule>();
