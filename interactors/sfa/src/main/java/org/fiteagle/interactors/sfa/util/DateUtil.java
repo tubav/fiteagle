e03b8741b3a48a329197e889c124971892ab20b6
dnehls <daniel.nehls@tu-berlin.de>
2014-06-27 16:32:44 +0200

    restored timezone for date format, otherwise tests fails

--- interactors/sfa/src/main/java/org/fiteagle/interactors/sfa/util/DateUtil.java
+++ interactors/sfa/src/main/java/org/fiteagle/interactors/sfa/util/DateUtil.java
@@ -7,11 +7,11 @@ import java.util.Date;
 public class DateUtil {
 
 	public static String getFormatedDate(Date expirationTime) {
-		 SimpleDateFormat rfc3339 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		 SimpleDateFormat rfc3339 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 			return rfc3339.format(expirationTime);
 		}
 	public static Date getDateFromString(String expirationTime) throws ParseException{
-		 SimpleDateFormat rfc3339 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		 SimpleDateFormat rfc3339 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 		 return rfc3339.parse(expirationTime);
 	}
 }
